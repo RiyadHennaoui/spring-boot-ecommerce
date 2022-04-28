d2259d869ab3b7357441f848395507c4efed8eea
Riyad Hennaoui <riyad.hennaoui@gmail.com>
2022-04-28 15:59:43 +0200

    @Pagination

--- src/main/java/com/luv2code/ecommerce/springbootecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/springbootecommerce/config/MyDataRestConfig.java
@@ -2,14 +2,28 @@ package com.luv2code.ecommerce.springbootecommerce.config;
 
 import com.luv2code.ecommerce.springbootecommerce.entity.Product;
 import com.luv2code.ecommerce.springbootecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -26,6 +40,23 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        exposeIds(config);
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType entityType : entities){
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
 
     }
 }
--- src/main/java/com/luv2code/ecommerce/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/springbootecommerce/dao/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.luv2code.ecommerce.springbootecommerce.dao;
 
 import com.luv2code.ecommerce.springbootecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Page<Product> findByCategoryId(@RequestParam("id") Integer id, Pageable pageable);
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
